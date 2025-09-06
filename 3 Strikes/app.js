3f7451b91f717a8c1157d7bcb582b33f6635f4ed
Gabriel <gabrielngoh07@gmail.com>
2025-09-06 08:26:04 +0100

    3 Strikes (3 solutions )

--- 3 Strikes/app.js
+++ 3 Strikes/app.js
@@ -9,3 +9,45 @@ function squaresWithThree(n) {
   }
   return count;
 }
+
+/**
+ * Solution 2 :
+ * 
+ * function squaresWithThree(n) {
+  let count = 0 ;
+  for (let i=1 ; i<=n;i+=1)
+  {
+    if (hasThree(`${i * i}`))
+    {
+      count+=1;
+    } 
+  }
+  return count;
+}
+
+const hasThree = (el)=> {
+  for (let i of el)
+  {
+    if (i == 3){
+      return true;
+    }
+  }
+  return false ;
+}
+ */
+/**
+ * Solution 3 : 
+ * 
+ * function squaresWithThree(n) {
+  let count = 0 ;
+  for (let i=1 ; i<=n;i+=1)
+  {
+    if (`${i * i}`.includes(3))
+    {
+      count+=1;
+    } 
+  }
+  return count;
+}
+
+ */
